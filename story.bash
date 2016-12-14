3cc051462722231d681c5464e84a713c8c377639
Alexey Melezhik <melezhik@gmail.com>
2016-12-14 13:11:31 +0300

    up

--- story.bash
+++ story.bash
@@ -14,11 +14,12 @@ git submodule --quiet sync || exit 1
 if test -z $project; then
 
   git submodule update --init --recursive --remote  || exit 1
+  git submodule foreach git branch -v || exit 1
 else
 
   echo updating $project ...
   git submodule --quiet update --init --remote $project || exit 1
-
+  git submodule status $project || exit 1
 fi
 
 git commit -a -m "update git submodules"
@@ -27,5 +28,4 @@ git pull || exit 1
 
 git push || exit 1
 
-git submodule foreach git branch || exit 1
 
